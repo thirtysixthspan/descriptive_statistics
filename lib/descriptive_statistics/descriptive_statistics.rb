4a3a394c155a0e0f27d374734b05e3070ad025c9
Graham Malmgren <gtmalmgren@gmail.com>
2013-07-02 07:16:37 -0400

    Added a new descriptive_statistics method in the DescriptiveStatistics module, which returns a hash containing the common values considered to be part of "descriptive statistics." Also added the associated tests."

---
+++ lib/descriptive_statistics/descriptive_statistics.rb
@@ -0,0 +1,17 @@
+module DescriptiveStatistics
+  def descriptive_statistics
+    return { :number => self.size.to_f,
+             :sum => self.sum,
+             :variance => self.variance,
+             :standard_deviation => self.standard_deviation,
+             :min => self.min,
+             :max => self.max,
+             :mean => self.mean,
+             :mode => self.mode,
+             :median => self.median,
+             :range => self.range,
+             :q1 => self.percentile(25),
+             :q2 => self.percentile(50),
+             :q3 => self.percentile(75) }
+  end
+end
