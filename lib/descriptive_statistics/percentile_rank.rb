f05e242b64ab5e1b0ce1f9be42820bcb009a980c
Guy Shechter <guy.shechter@philips.com>
2014-05-04 23:08:57 -0400

    Change result from [0,1] to [0,100]

--- lib/descriptive_statistics/percentile_rank.rb
+++ lib/descriptive_statistics/percentile_rank.rb
@@ -1,6 +1,6 @@
 module DescriptiveStatistics
   def percentile_rank(p)
     sorted = self.sort
-    return sorted.find_index{ |x| x >= p} / sorted.length.to_f
+    return sorted.find_index{ |x| x >= p} / sorted.length.to_f * 100.0
   end
 end
