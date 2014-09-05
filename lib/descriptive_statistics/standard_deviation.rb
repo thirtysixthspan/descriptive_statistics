cec1d52f7ab40e49e848c6773f35f6414ee01843
James Gibson <james@innovategsb.com>
2014-09-05 17:19:52 -0400

    Added Sample Variance & SD
    The existing methods calculate the 'population' variance and standard
    deviation. Added methods to calculate the 'sample' versions of theseA
    quantities, which provide unbiased estimates of the population
    parameters when a sample is used to estimate.

--- lib/descriptive_statistics/standard_deviation.rb
+++ lib/descriptive_statistics/standard_deviation.rb
@@ -5,4 +5,11 @@ module DescriptiveStatistics
 
     Math.sqrt(values.variance)
   end
+
+  def sample_standard_deviation(collection = self)
+    values = Support::convert(collection)
+    return unless values.size > 0
+
+    Math.sqrt(values.sample_variance)
+  end
 end
--- lib/descriptive_statistics/variance.rb
+++ lib/descriptive_statistics/variance.rb
@@ -6,4 +6,12 @@ module DescriptiveStatistics
     mean = values.mean
     values.map{ |sample| (mean - sample) ** 2 }.inject(:+) / values.number
   end
+
+  def sample_variance(collection = self)
+    values = Support::convert(collection)
+    return unless values.size > 0
+
+    mean = values.mean
+    values.map{ |sample| (mean - sample) ** 2 }.inject(:+) / [values.number - 1, 1].max
+  end
 end
