560cf5000ebb3411bff9170e2a1736b64a60e308
Charlie Egan <charlieegan3@googlemail.com>
2014-10-27 23:32:24 +0000

    Improve .map block spacing

--- lib/descriptive_statistics/percentile_rank.rb
+++ lib/descriptive_statistics/percentile_rank.rb
@@ -4,6 +4,6 @@ module DescriptiveStatistics
     values = Support::convert(collection)
     return DescriptiveStatistics.percentile_rank_empty_collection_default_value if values.empty?
 
-    return (((values.sort.rindex{ |x| x <= p } || -1.0) + 1.0)) / values.number * 100.0
+    return (((values.sort.rindex { |x| x <= p } || -1.0) + 1.0)) / values.number * 100.0
   end
 end
