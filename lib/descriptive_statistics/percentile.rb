8a8b533450bc78bf4f03aa967e39817c974e2c07
Derrick Parkhurst <derrick.parkhurst@gmail.com>
2014-11-15 15:03:52 -0600

    prefer guard clauses over if/else

--- lib/descriptive_statistics/percentile.rb
+++ lib/descriptive_statistics/percentile.rb
@@ -2,14 +2,13 @@ module DescriptiveStatistics
   def percentile(p, collection = self)
     values = Support::convert(collection)
 
-    if values.empty?; DescriptiveStatistics.percentile_empty_collection_default_value
-    elsif values.size == 1; values.first
-    else
-      values.sort!
-      return values.last if p == 100
-      rank = p / 100.0 * (values.size - 1)
-      lower, upper = values[rank.floor,2]
-      lower + (upper - lower) * (rank - rank.floor)
-    end
+    return DescriptiveStatistics.percentile_empty_collection_default_value if values.empty?
+    return values.first if values.size == 1
+
+    values.sort!
+    return values.last if p == 100
+    rank = p / 100.0 * (values.size - 1)
+    lower, upper = values[rank.floor,2]
+    lower + (upper - lower) * (rank - rank.floor)
   end
 end
