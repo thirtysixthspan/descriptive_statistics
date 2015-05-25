e27f6e8ab858e183397d8a5b07db41a3ccae3a12
Shawn Lee-Kwong <splitnova@gmail.com>
2015-05-25 12:10:25 -0400

    sample standard deviation

--- lib/descriptive_statistics/variance.rb
+++ lib/descriptive_statistics/variance.rb
@@ -1,9 +1,11 @@
 module DescriptiveStatistics
-  def variance(collection = self, &block)
+  def variance(s = false, collection = self, &block)
     values = Support::convert(collection, &block)
     return DescriptiveStatistics.variance_empty_collection_default_value if values.empty?
 
     mean = values.mean
-    values.map { |sample| (mean - sample) ** 2 }.reduce(:+) / values.number
+
+    number = s ? values.number - 1 : values.number
+    values.map { |sample| (mean - sample) ** 2 }.reduce(:+) / number
   end
 end
