3e04b29592bdb96a6ce6addad53cb916a3c8dc1c
Shawn Lee-Kwong <splitnova@gmail.com>
2015-05-25 12:25:18 -0400

    should raise error

--- spec/refinement/refinement_spec.rb
+++ spec/refinement/refinement_spec.rb
@@ -19,7 +19,7 @@ describe "DescriptiveStatistics" do
       end
     end
 
-    expect{Test.unrefined}.not_to raise_error
+    expect{Test.unrefined}.to raise_error
     expect{Test.refined}.not_to raise_error
   end
 
