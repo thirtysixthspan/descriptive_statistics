72d851090ddba7bca0a3e5a1376c689f7c0ce6a3
Lidiane Taquehara <lidi.mayra@gmail.com>
2016-07-14 07:04:33 -0300

    Broke conditional in lines (too long)

--- lib/descriptive_statistics/coefficient_of_variation.rb
+++ lib/descriptive_statistics/coefficient_of_variation.rb
@@ -1,7 +1,10 @@
 module DescriptiveStatistics
   def coefficient_of_variation(collection = self, &block)
     values = Support::convert(collection, &block)
-    return DescriptiveStatistics.coefficient_of_variation_empty_collection_default_value if values.empty?
+
+    if values.empty?
+      return DescriptiveStatistics.coefficient_of_variation_empty_collection_default_value
+    end
 
     values.standard_deviation / values.mean
   end
