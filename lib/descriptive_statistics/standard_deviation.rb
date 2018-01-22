92e69ddebc334fcb141da51c4c4d2d19cfa1edbd
esteban zapata <ezapata@altavistaed.com>
2018-01-22 12:59:57 -0500

    variance & std: Return the expected default value for numbers: 0.0.
    Due to the fix in the variance formula, we need to early return
    the expected value for single numbers, which is 0.0. This value
    was generated because of (x - x.mean) in the variance formula, which for
    single numbers is 0, so 0/anything is zero.

--- lib/descriptive_statistics/standard_deviation.rb
+++ lib/descriptive_statistics/standard_deviation.rb
@@ -2,6 +2,7 @@ module DescriptiveStatistics
   def standard_deviation(collection = self, &block)
     values = Support::convert(collection, &block)
     return DescriptiveStatistics.standard_deviation_empty_collection_default_value if values.empty?
+    return 0.0 if values.number == 1 # Early return to avoid NaN values.
 
     Math.sqrt(values.variance)
   end
